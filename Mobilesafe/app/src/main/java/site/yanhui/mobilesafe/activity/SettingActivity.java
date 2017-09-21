d5ac7c1c67b70e42ee66d79bb1ff7e4449a4a511
Archer <421228286@qq.com>
2017-09-21 08:51:59 +0800

    使用sp存储更新状态

--- Mobilesafe/app/src/main/java/site/yanhui/mobilesafe/activity/SettingActivity.java
+++ Mobilesafe/app/src/main/java/site/yanhui/mobilesafe/activity/SettingActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import site.yanhui.mobilesafe.R;
+import site.yanhui.mobilesafe.other.ConstantValue;
+import site.yanhui.mobilesafe.utils.SpUtils;
 import site.yanhui.mobilesafe.view.SettingItemView;
 
 public class SettingActivity extends AppCompatActivity  {
@@ -22,6 +24,12 @@ public class SettingActivity extends AppCompatActivity  {
      */
     private void initUpdate() {
         final SettingItemView siv_update = (SettingItemView) findViewById(R.id.siv_update);
+
+        //获取已经有的开关状态，更新app。//默认false
+        boolean update = SpUtils.getBoolean(this, ConstantValue.OPEN_UPDATE, false);
+        //是否选中，根据上次的结果做判断。
+        siv_update.setCheck(update);
+
         siv_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +37,9 @@ public class SettingActivity extends AppCompatActivity  {
                 boolean check = siv_update.isCheck();//拿到默认选中状态
                //设置取反
                 siv_update.setCheck(!check);//设置为取反
+
+                //把点击时间传入config里面，下次再打开的时候直接读取
+                SpUtils.putBoolean(getApplicationContext(),ConstantValue.OPEN_UPDATE,!check);
             }
         });
     }
---
+++ Mobilesafe/app/src/main/java/site/yanhui/mobilesafe/other/ConstantValue.java
@@ -0,0 +1,16 @@
+package site.yanhui.mobilesafe.other;
+
+/**
+ * Created by Archer on 2017/9/21.
+ * <p>
+ * 功能描述：
+ * 专门建立一个类用来，保存项目中用到的常量
+ */
+
+public class ConstantValue {
+
+    /**
+     * 用于读取，是否启动自动更新的节点
+     */
+    public static  final String OPEN_UPDATE = "open_date";
+}
